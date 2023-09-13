8f17624f0985599a0b7ce777cb2c3e94f6a69f04
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-09-13 23:37:18 +0000

    removed unnecessary jumbotron class

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -5,7 +5,7 @@ const Jumbotron = () => {
     return (
             <div className="row g-0">
                 <div className="col-12">
-                    <div className="jumbotron bg-light p-4 mt-4">
+                    <div className="bg-light p-4 mt-4">
                         <h1 className="display-4"><strong>A Warm Welcome!</strong></h1>
                         <h5 className="noBold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</h5>
                         <p className="lead p-3">
